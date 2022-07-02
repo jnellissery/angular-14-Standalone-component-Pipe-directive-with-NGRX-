25c95e0e8b853c4c15392e403bc232fe77d0ab50
jnellissery <jnellissery@yahoo.com>
2022-07-02 09:36:27 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule, OnInit } from '@angular/core';
-import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ControlBase } from './controls/control-base';
-import { DataService } from './data.service';
 import { UserDirective } from './directives/user.directive';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { DynamiccompService } from './dynamiccomp.service';
-import { MetaService } from './meta.service';
+
 import { UserPipe } from './pipes/user.pipe';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, CommonModule, UserDirective, UserPipe, FormsModule, DynamicFormComponent],
+  imports: [
+    RouterModule,
+    CommonModule,
+    UserDirective,
+    UserPipe,
+    FormsModule,
+    DynamicFormComponent,
+  ],
   standalone: true,
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'dynamic-form-app';
 
-  constructor(
-  ) {}
- 
+  constructor() {}
 }
